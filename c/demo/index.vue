4b09ba708b3e04436f1ea89cbed8e1706cf536b1
zhangchunlei <15116991665@163.com>
2020-12-28 16:30:30 +0800

    init

--- c/demo/index.vue
+++ c/demo/index.vue
@@ -1,27 +1,13 @@
 <template>
-  <view>
-    DatePicker示例
-    <DatePicker :value="date"/>
-    <button @click="open">打开DatePicker弹窗</button>
-    <uni-popup ref="popup" type="bottom">
-      <DatePicker :value="date"/>
-    </uni-popup>
-  </view>
 </template>
 
 <script>
-import DatePicker from './date-picker.vue';
-import { uniPopup } from '@dcloudio/uni-ui'
-
 export default {
   data() {
     return {
-      date: [1, 2, 3]
     };
   },
   components: {
-    DatePicker,
-    uniPopup,
   },
   onShow() {
   },
@@ -32,9 +18,6 @@ export default {
   onShareAppMessage() {
   },
   methods: {
-    open(){
-        this.$refs.popup.open()
-    }
   }
 };
 </script>
